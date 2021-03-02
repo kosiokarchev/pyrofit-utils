fix broadcast_except

pyrofit/utils/torchutils.py
@@ -16,7 +16,7 @@ def map_location(obj, location=None):
 
 def broadcast_except(*tensors: torch.Tensor, dim=-1):
     shape = torch.broadcast_tensors(*[t.select(dim, 0) for t in tensors])[0].shape
-    return [t.expand(*shape[:dim+1], t.shape[dim], *shape[dim+1:]) for t in tensors]
+    return [t.expand(*shape[:t.ndim + dim + 1], t.shape[dim], *shape[t.ndim + dim + 1:]) for t in tensors]
 
 
 # TODO: improve so that nbatch=-1 means "auto-derive nbatch from number of